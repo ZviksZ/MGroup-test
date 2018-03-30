f8a25c1c9d0854526351df76584761518bfc0b78
Nikita Kapshtyk <viks2332@gmail.com>
2018-03-30 23:22:52 +0300

    JS

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,23 @@
+$(document).ready(function(){
+    $("#btn_calendar").click(function(){
+        $(".content-btn__add").slideToggle("slow");
+        return false;
+    });
+});
 
+
+$(document).ready(function(){
+    $(".header-city__date").click(function(){
+    	$("#header-search").hide();
+        $("#header-date").slideToggle("slow");
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $(".header-city__search").click(function(){
+    	$("#header-date").hide();
+        $("#header-search").slideToggle("slow");
+        return false;
+    });
+});
